ad25af06bc9852c3e1ce682a7c95db423282c17c
V1K1NGbg <victor@ilchev.com>
2024-09-18 23:05:22 +0200

    "nvidia"

--- hosts/nixosbtw/configuration.nix
+++ hosts/nixosbtw/configuration.nix
@@ -9,6 +9,7 @@
       [ # Include the results of the hardware scan.
          ./hardware-configuration.nix
          inputs.home-manager.nixosModules.home-manager
+         # ./../../modules/nixos/nvidia.nix
       ];
 
    nix.settings.experimental-features = [ "nix-command" "flakes" ];
---
+++ modules/nixos/nvidia.nix
@@ -0,0 +1,15 @@
+{ config, ... }:
+
+{
+	services.xserver.videoDrivers = [ "nvidia" ];
+
+	hardware.nvidia = {
+      modesetting.enable = true;
+      powerManagement.enable = true;
+      powerManagement.finegrained = false;
+      open = false;
+      nvidiaSettings = true;
+
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+	};
+}
